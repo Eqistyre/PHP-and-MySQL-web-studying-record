6071043c44efc8a64b527a5f0a388429f9e9c033
Eqistyre <stillalive@aliyun.com>
2015-09-13 14:37:46 +0800

    增加日期显示与条件判断

--- 第一章/订单/php/processorder.php
+++ 第一章/订单/php/processorder.php
@@ -10,6 +10,7 @@
         $jiaGe = $kaFei * KAFEI
                + $jiDing * JIDING
                + $huiGuo * HUIGUO;
+        $date = date('H:i,jS F Y');
 ?>
 
 <html>
@@ -19,13 +20,35 @@
 </head>
 <body>
         <h1>请确认你的订单</h1>
-        <?php
-                echo '<p> 总计</p>';
-                echo $kaFei.'咖啡</br>';
-                echo $jiDing.'宫爆鸡丁</br>';
-                echo $huiGuo.'回锅肉</br>';
-                echo '价格：'.$jiaGe.'元</br>';
-        ?>
+   			<?php
+
+	        if (true) {
+	        	echo 'hello';
+
+	                echo '<p> 总计</p>';
+
+	                if ($kaFei > 0) {
+         			   	echo $kaFei.'咖啡</br>';
+	                }
+
+	                if ($jiDing > 0) {
+	                	echo $jiDing.'宫爆鸡丁</br>';
+	            	}
+
+	            	if ($huiGuo > 0) {
+	                	echo $huiGuo.'回锅肉</br>';
+	            	}
+
+	                echo '价格：'.$jiaGe.'元</br>';
+
+	                 echo "<p>订单创建于".date('H:i,jS F Y')."</p>";
+
+	            }
+	        	else {
+	        		echo '你没有购买任何物品';
+	        	}
+
+       		 ?>
 </body>
 </html>
               
